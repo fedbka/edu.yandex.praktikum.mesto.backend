51e8c62d120718a8914742eb86a87cb3915a2b5b
Andrey Fadeyev <fadeyeval@outlook.com>
2024-05-07 22:15:12 +0500

    feature: add eslint & prettier configs

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,9 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const _id = 'test';
+
 app.listen(PORT, () => {
-    // Если всё работает, консоль покажет, какой порт приложение слушает
-    console.log(`App listening on port ${PORT}`)
-})
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT} ${_id}`);
+});
