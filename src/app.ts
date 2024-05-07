2d477fdc9e77f0ea43c2d031ab531ce55f1102b6
Andrey Fadeyev <fadeyeval@outlook.com>
2024-05-07 22:36:39 +0500

    fix: eslint configs and add build command

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,5 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
